cfd46ad1027495dc5148782f3c1e8d3014f7a670
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-17 23:46:30 +0100

    Vuejs form transmits events to server

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ app.get('/', function(request, response){
 
 io.on('connection', function(socket){
 	console.log('A connection was made ')
+    socket.on('chat.message', function(message){
+		console.log('New message: ' + message)
+	})
 })
