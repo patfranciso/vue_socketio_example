60607616e5eaf496a777cfadd8cf7f878713b50a
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-17 23:35:41 +0100

    Simple index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+var app = require('express')()
+
+app.listen(3000, ()=>console.log("Listening on port 3000"))	// to log add a labda
+
+app.get('/', function(request, response){
+	response.send('Hello')
+})
