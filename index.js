1eebc14b77ad110b8af9dafaea6f9a969762ee76
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-17 23:37:32 +0100

    Rendering a file template

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var app = require('express')()
 app.listen(3000, ()=>console.log("Listening on port 3000"))	// to log add a labda
 
 app.get('/', function(request, response){
-	response.send('Hello')
+	response.sendFile(__dirname + '/index.html')
 })
