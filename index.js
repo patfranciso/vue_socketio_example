69cab84114204116a942f76301cf111a92b70920
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-05-17 23:41:45 +0100

    Socket.io connect success

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var app = require('express')()
+const app = require('express')()
+const server = require('http').createServer(app);
+var io = require('socket.io')(server)
 
-app.listen(3000, ()=>console.log("Listening on port 3000"))	// to log add a labda
+
+server.listen(3000, ()=>console.log("Listening on port 3000"))	// to log add a labda
 
 app.get('/', function(request, response){
 	response.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+	console.log('A connection was made ')
 })
